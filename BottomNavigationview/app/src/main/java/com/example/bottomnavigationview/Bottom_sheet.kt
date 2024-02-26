1791b00e768ec9c26f14d9193a3c5f13821f3db4
sukritivats <technicalloop10@gmail.com>
2024-02-26 12:41:24 +0530

    Commit msg

---
+++ BottomNavigationview/app/src/main/java/com/example/bottomnavigationview/Bottom_sheet.kt
@@ -0,0 +1,18 @@
+//package layout
+//
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+//
+//class bottom_sheet:BottomSheetDialogFragment() {
+//    private lateinit var binding:B
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        return super.onCreateView(inflater, container, savedInstanceState)
+//    }
+//}
